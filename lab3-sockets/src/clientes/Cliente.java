50bb786ff23e522c4b429cf1be3a6fcb241a1c02
Julian Oliveros Forero <je.oliverosf@uniandes.edu.co>
2021-03-11 19:46:15 -0500

    Cliente-servidor multithread funcionando

--- lab3-sockets/src/clientes/Cliente.java
+++ lab3-sockets/src/clientes/Cliente.java
@@ -1,70 +1,69 @@
-//package clientes;
+package clientes;
 
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class Cliente {
-	
-//	private int idCliente;
-//	
-//	
-//	public Cliente(int id) {
-//		this.idCliente=id;
-//	}
-	
-	
-	
-	
-    public static void main(String[] args) {
 
-        //Host del servidor
-        final String HOST = "localhost";
+	public static void main(String[] args) {
 
-        //Puerto del servidor
-        final int PUERTO =60000;
-                
-        DataInputStream in;
-        DataOutputStream out;
+		//Host del servidor
+		final String HOST = "localhost";
 
-        try {
-        	
-            //Creo el socket para conectarme con el cliente
-            Socket sc = new Socket(HOST, PUERTO);
+		//Puerto del servidor
+		final int PUERTO =60000;
+
+		PrintWriter out = null; 
+		BufferedReader in = null; 
+
+		try {
+			Socket sc = new Socket(HOST, PUERTO);
+
+			// Escribir a el servidor
+			out = new PrintWriter( sc.getOutputStream(), true); 
+
+			// Leer del servidor 
+			in = new BufferedReader(new InputStreamReader( sc.getInputStream())); 
 
 
-            //recibe datos
-            in = new DataInputStream(sc.getInputStream());
-            
-            //envio de datos
-            out = new DataOutputStream(sc.getOutputStream());
+			// Intercambio de texto
+			Scanner scaner = new Scanner(System.in); 
+			String line = null; 
+
+			while (!"exit".equalsIgnoreCase(line)) { 
+
+				// Leer por consola
+				line = scaner.nextLine(); 
+
+				// Enviar de usuario a servidor
+				out.println(line); 
+				out.flush(); 
+
+				// displaying server reply 
+				System.out.println("Server replied " + in.readLine()); 
+			} 
 
 
-            //Envio un mensaje al cliente
-            out.writeUTF("¡Hola mundo desde el cliente!");
+			// cerrar socket
+			sc.close(); 
 
 
-            //Recibo el mensaje del servidor
-            String mensaje = in.readUTF();
+		} catch (IOException ex) {
 
-            //imprimo mensaje del servidor
-            System.out.println(mensaje);
+			Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-            
-            //cierro la conexion
-            sc.close();
-
-        } catch (IOException ex) {
-        	
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
+	}
 
 }
 
--- lab3-sockets/src/servidores/Servidor.java
+++ lab3-sockets/src/servidores/Servidor.java
@@ -3,167 +3,134 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Servidor {
-	
-	
-	
+
+
+
 	/**
 	 * Clase que crear un thread por cada peticion
 	 * @author julianoliveros
 	 *
 	 */
-	
-	/*
 	private static class Peticion extends Thread{
-		
+
 		private final Socket clienteSC;
-		
+
 		public Peticion(Socket sc) {
 			this.clienteSC=sc;
 		}
-		
+
 		public void run() 
-        {
-            DataInputStream in = null;
-            DataOutputStream out = null;
-            
-            try { 
-            	
-                //recino de datos
-                in = new DataInputStream(clienteSC.getInputStream());
-                
-                //envio de datos
-                out = new DataOutputStream(clienteSC.getOutputStream());
+		{
+			PrintWriter out = null; 
+			BufferedReader in = null; 
 
-                //Leo el mensaje que me envia
-                String mensaje = in.readUTF();
+			try { 
 
-                System.out.println(mensaje);
+				// Ecribir a el cliente
+				out = new PrintWriter( clienteSC.getOutputStream(), true); 
 
-                //Le envio un mensaje
-                out.writeUTF("¡Hola mundo desde el servidor!");
-
-                clienteSC.close();
-                System.out.println("Cliente desconectado");
-                                  
-                  // get the outputstream of client 
-            	
-                out = new PrintWriter( 
-                		clienteSC.getOutputStream(), true); 
-  
-                  // get the inputstream of client 
-                
-                in = new BufferedReader( 
-                    new InputStreamReader( 
-                    		clienteSC.getInputStream())); 
-  
-                String line; 
-                
-                while ((line = in.readLine()) != null) { 
-  
-                    // writing the received message from 
-                    // client 
-                    System.out.printf( 
-                        " Sent from the client: %s\n", 
-                        line); 
-                    out.println(line);    
-                } 
-                
-                
-                
-            } 
-            catch (IOException e) { 
-                e.printStackTrace(); 
-            } 
-            
-            finally { 
-                try { 
-                    if (out != null) { 
-                        out.close(); 
-                    } 
-                    if (in != null) { 
-                        in.close(); 
-
-                        
-                    } 
-                } 
-                catch (IOException e) { 
-                    e.printStackTrace(); 
-                } 
-            } 
-        } 
-    } 
-		*/
-		
-		
-	
-
-    public static void main(String[] args) {
-
-        ServerSocket servidor = null;
-        
-        DataInputStream in = null;
-        DataOutputStream out = null;
-        Socket clienteSC=null;
-
-        int numeroDeClientes=0;
-        final int PUERTO =60000;
+				// Leer del cliente 
+				in = new BufferedReader( new InputStreamReader(clienteSC.getInputStream())); 
 
 
-        try {
-        	
-            //Creamos el socket del servidor
-            servidor = new ServerSocket(PUERTO);
-            
-            //Permite que se se pueda utilizar el mismo puerto
-            //servidor.setReuseAddress(true);
-            System.out.println("Servidor iniciado");
 
-            
-            //Siempre estara escuchando peticiones
-            while (true) {
+				Scanner scaner = new Scanner(System.in); 
+				
+				String line; 
+				while ((line = in.readLine()) != null) { 
 
-            	
-               //Espero a que un cliente se conecte
-              clienteSC = servidor.accept();
-              
-              System.out.println("Cliente conectado"+ clienteSC.getInetAddress().getHostAddress());
-                
-                
-              //recino de datos
-              in = new DataInputStream(clienteSC.getInputStream());
-              
-              //envio de datos
-              out = new DataOutputStream(clienteSC.getOutputStream());
+					// Escribiendo el mesanje del cliente
+					System.out.printf( " Sent from the client: %s\n", line); 
+					
+					out.println(line); 
+				} 
 
-              //Leo el mensaje que me envia
-              String mensaje = in.readUTF();
 
-              System.out.println(mensaje);
+				clienteSC.close();
+				System.out.println("Cliente desconectado");
 
-              //Le envio un mensaje
-              out.writeUTF("¡Hola mundo desde el servidor!");
+				
 
-              clienteSC.close();
-              System.out.println("Cliente desconectado");
-                
-                
-//                Peticion threadCliente = new Peticion(sc); 
-//                threadCliente.start();
-                
-                numeroDeClientes++;
-               
-            }
+			} 
+			catch (IOException e) { 
+				e.printStackTrace(); 
+			} 
 
-        }
-        catch (IOException ex)
-            {
-                Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
-            }
-    }
+			finally { 
+				try { 
+					if (out != null) { 
+						out.close(); 
+					} 
+					if (in != null) { 
+						in.close();  
+					} 
+				} 
+				catch (IOException e) { 
+					e.printStackTrace(); 
+				} 
+			} 
+		} 
+	} 
+
+
+
+
+
+	public static void main(String[] args) {
+
+		ServerSocket servidor = null;
+
+		//        DataInputStream in = null;
+		//        DataOutputStream out = null;
+
+		int numeroDeClientes=0;
+		final int PUERTO =60000;
+
+
+		try {
+
+			//Creamos el socket del servidor
+			servidor = new ServerSocket(PUERTO);
+
+			//Permite que se se pueda utilizar el mismo puerto
+			servidor.setReuseAddress(true);
+			System.out.println("Servidor iniciado");
+
+
+			//Siempre estara escuchando peticiones
+			while (true) {
+
+
+				//Espero a que un cliente se conecte
+				Socket clienteSC = servidor.accept();
+
+				System.out.println("Cliente conectado"+ clienteSC.getInetAddress().getHostAddress());
+
+				Peticion threadCliente = new Peticion(clienteSC); 
+				threadCliente.start();
+
+				numeroDeClientes++;
+
+			}
+		}
+		catch (IOException ex)
+		{
+			Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+		} finally {
+			if(servidor!=null) {
+				try {
+					servidor.close();
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 }
 
