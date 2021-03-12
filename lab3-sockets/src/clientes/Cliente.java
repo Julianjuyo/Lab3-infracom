b990734715056f47ffd884caeb9f2d8ffd50ba23
Julian Oliveros Forero <je.oliverosf@uniandes.edu.co>
2021-03-12 07:24:20 -0500

    hash cliente

--- lab3-sockets/src/clientes/Cliente.java
+++ lab3-sockets/src/clientes/Cliente.java
@@ -12,6 +12,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -22,31 +23,50 @@ public class Cliente {
 	
 	
 	/**
-	 * 
+	 * Convierte en un arreglo de bits un archivo 
+	 * @param file
+	 * @return
+	 */	
+	public static byte[] getArray(File file){
+		byte[] byteArray = new byte[(int) file.length()];
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			int bytesCount = 0; 
+			bytesCount = fis.read(byteArray);
+			fis.close();
+			System.out.println("El archivo " + file.getName() + " tiene " + bytesCount +" bytes.");
+
+		} catch (Exception e) {
+			System.out.println("Problemas al convertir el archivo a bytes: "+ e.getMessage());
+		}
+		return byteArray;
+	}
+
+	/**
+	 * Funacion que crea un hash a partir de un archivo 
 	 * @param file
 	 * @return
 	 */
-	public String getHash(File file){
-		String r="";
-		MessageDigest md5 = null;
-		try {
-			md5 = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			FileInputStream fis = new FileInputStream(file);
-			DigestInputStream dis = new DigestInputStream(fis, md5);
-			r=dis.toString(); 
-			fis.close();
-			System.out.println("El hash de confirmación md5 del archivo " + file.getName() + "es: /n " + r);
-			
-		} catch (Exception e) {
-			System.out.println("Problemas al convertir el archivo a hash md5: "+ e.getMessage());
-		}
-		return  r;
-	}
+	public static String getHash(File input) 
+	{ 
+		try { 
+			MessageDigest md = MessageDigest.getInstance("MD5"); 
+
+			byte[] messageDigest = md.digest(getArray(input)); 
+
+			BigInteger no = new BigInteger(1, messageDigest); 
+
+			String hashtext = no.toString(16); 
+			while (hashtext.length() < 32) { 
+				hashtext = "0" + hashtext; 
+			} 
+			return hashtext; 
+		} 
+
+		catch (NoSuchAlgorithmException e) { 
+			throw new RuntimeException(e); 
+		} 
+	} 
 	
 	
 
