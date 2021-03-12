8726bd0dd5bc7c25a18ef15f01544675644f3c6a
Julian Oliveros Forero <je.oliverosf@uniandes.edu.co>
2021-03-11 23:36:21 -0500

    Ya funciona TCP con carga de datos

--- lab3-sockets/src/clientes/Cliente.java
+++ lab3-sockets/src/clientes/Cliente.java
@@ -1,11 +1,16 @@
-package clientes;
+//package clientes;
 
 
 import java.io.BufferedReader;
+import java.security.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -14,6 +19,40 @@ import java.util.logging.Logger;
 
 
 public class Cliente {
+	
+	
+	private static String getFileChecksum(MessageDigest digest, File file) throws IOException
+	{
+	    //Get file input stream for reading the file content
+	    FileInputStream fis = new FileInputStream(file);
+	     
+	    //Create byte array to read data in chunks
+	    byte[] byteArray = new byte[1024];
+	    int bytesCount = 0; 
+	      
+	    //Read file data and update in message digest
+	    while ((bytesCount = fis.read(byteArray)) != -1) {
+	        digest.update(byteArray, 0, bytesCount);
+	    };
+	     
+	    //close the stream; We don't need it now.
+	    fis.close();
+	     
+	    //Get the hash's bytes
+	    byte[] bytes = digest.digest();
+	     
+	    //This bytes[] has bytes in decimal format;
+	    //Convert it to hexadecimal format
+	    StringBuilder sb = new StringBuilder();
+	    for(int i=0; i< bytes.length ;i++)
+	    {
+	        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	    }
+	     
+	    //return complete hash
+	   return sb.toString();
+	}
+	
 
 	public static void main(String[] args) {
 
@@ -21,21 +60,45 @@ public class Cliente {
 		final String HOST = "localhost";
 
 		//Puerto del servidor
-		final int PUERTO =60000;
+		final int PUERTO =61001;
 
 		PrintWriter out = null; 
 		BufferedReader in = null; 
+		
+
 
 		try {
 			Socket sc = new Socket(HOST, PUERTO);
 
 			// Escribir a el servidor
 			out = new PrintWriter( sc.getOutputStream(), true); 
+	
 
 			// Leer del servidor 
 			in = new BufferedReader(new InputStreamReader( sc.getInputStream())); 
+			
+			
+			
+			
+			//TRANSFERENCIA DE ARCHIVOS
+			/*
+		    DataOutputStream outD = new DataOutputStream(new OutputStream(sc.getOutputStream()));
+		    DataInputStream inD = new DataInputStream(new InputStream(sc.getInputStream()));
+		   
+		    byte[] bytes = new byte[1024];
 
+		    inD.read(bytes);
+		    System.out.println(bytes);
 
+		    FileOutputStream fos = new FileOutputStream("C:\\test2.xml");
+		    fos.write(bytes);
+		    
+*/
+		    
+		    
+			
+		    
+			
 			// Intercambio de texto
 			Scanner scaner = new Scanner(System.in); 
 			String line = null; 
--- lab3-sockets/src/servidores/Servidor.java
+++ lab3-sockets/src/servidores/Servidor.java
@@ -6,11 +6,37 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.security.*;
 
 public class Servidor {
 
+	private final static String RUTA1="/Users/julianoliveros/100MBcopy.bin";
+	private final static String RUTA2="/Users/julianoliveros/250MBcopy.zip";
+	private static File fichero;
+	
 
-
+	/**
+	 * 
+	 * @param file
+	 * @return
+	 */
+	public static byte[] getArray (File file){
+		
+		byte[] byteArray = new byte[1024];
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			int bytesCount = 0; 
+			fis.read(byteArray);
+			fis.close();
+			
+		} catch (Exception e) {
+			System.out.println("Problemas al convertir el archivo a bytes: "+ e.getMessage());
+		}
+		return byteArray;
+	}
+	
+	
+	
 	/**
 	 * Clase que crear un thread por cada peticion
 	 * @author julianoliveros
@@ -19,30 +45,55 @@ public class Servidor {
 	private static class Peticion extends Thread{
 
 		private final Socket clienteSC;
+		private final byte[] arregloBits;
+		private final String log;
+		
 
-		public Peticion(Socket sc) {
+		public Peticion(Socket sc, byte[] parregloBits,String plog  ) {
 			this.clienteSC=sc;
+			this.arregloBits= parregloBits;
+			this.log=plog;
 		}
 
 		public void run() 
 		{
 			PrintWriter out = null; 
 			BufferedReader in = null; 
+			
+		    //DataOutputStream outD = null;
+		    //DataInputStream inD = null;
+		    
 
 			try { 
-
+				
+				
 				// Ecribir a el cliente
 				out = new PrintWriter( clienteSC.getOutputStream(), true); 
+				//outD = new DataOutputStream(clienteSC.getOutputStream());
 
 				// Leer del cliente 
 				in = new BufferedReader( new InputStreamReader(clienteSC.getInputStream())); 
-
-
-
-				Scanner scaner = new Scanner(System.in); 
+				//inD = new DataInputStream(clienteSC.getInputStream());
 				
+				
+				//TRANSFERENCIA DE ARCHIVOS
+				/*
+			    File file = new File("C:\\test.xml");
+			    InputStream is = new FileInputStream(file);
+			    //Get the size of the file
+			    long length = file.length();
+			    if (length > Integer.MAX_VALUE) {
+			        System.out.println("File is too large.");
+			    }
+			    byte[] bytes = new byte[(int) length];
+
+			    //out.write(bytes);
+			    System.out.println(bytes);
+				*/
+	
 				String line; 
-				while ((line = in.readLine()) != null) { 
+				while ((line = in.readLine()) != null) {
+					
 
 					// Escribiendo el mesanje del cliente
 					System.out.printf( " Sent from the client: %s\n", line); 
@@ -50,12 +101,10 @@ public class Servidor {
 					out.println(line); 
 				} 
 
-
+				
+				
 				clienteSC.close();
 				System.out.println("Cliente desconectado");
-
-				
-
 			} 
 			catch (IOException e) { 
 				e.printStackTrace(); 
@@ -78,6 +127,7 @@ public class Servidor {
 	} 
 
 
+	
 
 
 
@@ -85,25 +135,98 @@ public class Servidor {
 
 		ServerSocket servidor = null;
 
-		//        DataInputStream in = null;
-		//        DataOutputStream out = null;
-
 		int numeroDeClientes=0;
-		final int PUERTO =60000;
+		final int PUERTO =61001;
+		
+		String ruta=" ";
+		int numeroConexiones=0;
 
 
 		try {
-
+			
+			boolean CargoDatos=false;
+			
+			while(CargoDatos==false) {
+				
+				//Seleccionar que archivo quiere enviar
+				Scanner scaner = new Scanner(System.in);
+				
+				System.out.println("\n"+"Indique el numero de clientes a los que archivo quiere enviar el archivo \n");
+				
+				numeroConexiones = Integer.parseInt(scaner.nextLine());
+				
+				System.out.println(
+						"Indique que archivo quiere enviar (ESCRIBA EL NUMERO 1,2,3) \n"+
+								"1: archivo de 100 MB \n"+
+								"2: Archivo de 250 MB \n"+
+								"3: Otro (pasar ruta por parametro) \n"
+						);
+				
+				String Archivo = scaner.nextLine();
+				
+				//Transferir archivo de 100MB
+				if(Archivo.equals("1")) {
+					ruta= RUTA1;
+					fichero = new File(ruta);
+					if(fichero.exists()) { 
+						CargoDatos=true;
+						System.out.println("Se cargo el archivo correctamente \n");
+					}			
+				}
+				
+				//Transferir archivo de 250MB
+				else if(Archivo.equals("1")) {
+					ruta= RUTA1;
+					fichero = new File(ruta);
+					if(fichero.exists()) { 
+						CargoDatos=true;
+						System.out.println("Se cargo el archivo correctamente \n");
+					}
+				}
+				//Transferir otro archivo
+				else if(Archivo.equals("3")) {
+					boolean c=true;
+					while(c) {
+						System.out.println("Escriba la ruta del archivo \n");
+						ruta = scaner.nextLine();
+						fichero = new File(ruta);
+						
+						if(fichero.exists()) { 
+							CargoDatos=true;
+							c=false;
+							System.out.println("Se cargo el archivo correctamente \n");
+						}
+						else {
+							System.out.println("Ruta inavlida \n");							
+						}
+					}
+				}
+			}
+			
+			
 			//Creamos el socket del servidor
 			servidor = new ServerSocket(PUERTO);
 
 			//Permite que se se pueda utilizar el mismo puerto
 			servidor.setReuseAddress(true);
-			System.out.println("Servidor iniciado");
+			System.out.println("---------------- \n" +"Servidor iniciado \n");
 
-
+			
+			
+			
+			//se crea log
+			
+			//Realizar hash 
+			
+			//arreglo de bits 
+			
+			 byte[] arregloBits = getArray(fichero);
+			 
+			
 			//Siempre estara escuchando peticiones
-			while (true) {
+			
+			
+			while (numeroDeClientes < numeroConexiones) {
 
 
 				//Espero a que un cliente se conecte
@@ -111,12 +234,20 @@ public class Servidor {
 
 				System.out.println("Cliente conectado"+ clienteSC.getInetAddress().getHostAddress());
 
-				Peticion threadCliente = new Peticion(clienteSC); 
+				Peticion threadCliente = new Peticion(clienteSC,arregloBits,"aa"); //hash tambien envio, log 
 				threadCliente.start();
 
 				numeroDeClientes++;
 
 			}
+			
+			
+
+			
+			
+			//imprime log
+			
+			
 		}
 		catch (IOException ex)
 		{
