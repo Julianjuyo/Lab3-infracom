fae55eefa984662f089d98b9fc502b8f31369fa9
Ctrl Industries <ctrl.ind.sas@gmail.com>
2021-03-21 15:23:49 -0500

    Logger

--- lab3-sockets/src/util/Logger.java
+++ lab3-sockets/src/util/Logger.java
@@ -10,24 +10,30 @@ import java.util.Date;
 
 public class Logger {
 	
-	private String path;
+	private String PATH="/logs/";
 	private boolean append_to_file = false;
+	private String name  ;
 	
-	public Logger(String path) {
-		this.path = path;
-	}
 	
-	public Logger( String path , boolean append_to_file ) {
-		this.path = path;
-		this.append_to_file = append_to_file;
+	public Logger(int numRequest ) {
+		long millis= System.currentTimeMillis();
+		Date date= new Date(millis);
+		this.name= date.getYear() + "-" ;
+		this.name += date.getMonth() + "-";
+		this.name += date.getDay() +"-";
+		this.name += date.getHours() +"-";
+		this.name += date.getMinutes() +"-";
+		this.name += date.getSeconds() +".txt";
+
+
 	}
 	
 	public void log(String msg) throws IOException{
 		
-		FileWriter write = new FileWriter(path, append_to_file);
+		FileWriter write = new FileWriter(PATH + "this.name", append_to_file);
 		PrintWriter print_line= new PrintWriter(write);
 		
-		long millis=System. currentTimeMillis();
+		long millis=System.currentTimeMillis();
 		Date date= new Date(millis);
 		
 		print_line.println("/ "+ date);
@@ -35,32 +41,5 @@ public class Logger {
 		
 		print_line.close();
 	}
-	
-	
-//	public static void main(String[] args) {
-//		
-//		  String string = "0";
-//		  byte[] bytes = string.getBytes(Charset.defaultCharset());
-//		  System.out.println("String: " + string);
-//		  System.out.println("Bytes: " + Arrays.toString(bytes));
-//
-//		  
-//		  
-//		  String s = "4";
-//		  byte[] b = s.getBytes(StandardCharsets.UTF_8);
-//		  
-//		  System.out.println(s);
-//		  System.out.println(b[0]);
-//		  
-//		  
-//
-//		  byte[] bb = {(byte) 37};
-//		  String ss = new String(bb, StandardCharsets.US_ASCII);
-//		  
-//		  System.out.println(bb[0]);
-//		  System.out.println(ss);
-//		  
-//		  
-//	}
 
 }
